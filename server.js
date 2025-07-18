7de82b535bd609e3bf9d83e8452b4df91992387d
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-07-18 09:04:46 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+app.set("trust proxy", true);
+
+app.post('/set', (req, res) => {
+  const variable = req.body.var;
+  const value = req.body.data;
+  var db = fs.readFileSync('db.json', 'utf8');
+  db = JSON.parse(db);
+  db[variable].data = value;
+  db = JSON.stringify(db);
+  fs.writeFileSync('db.json', db);
+  res.send(200);
+});
+
+app.post('/data', (req, res) => {
+  const variable = req.body.var;
+  var db = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  res.send(
